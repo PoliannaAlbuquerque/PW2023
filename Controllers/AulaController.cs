4c73307a88d44245b9f8c04b5a7a6088116aaf95
PoliannaAlbuquerque <101806633+PoliannaAlbuquerque@users.noreply.github.com>
2023-03-20 00:28:01 -0300

    Add files via upload

---
+++ Controllers/AulaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExeLevi_Polianna1.Controllers
+{
+    public class AulaController : Controller
+    {
+        // GET: Aula
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Redirecionamento()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult Aluno()
+        {
+            ViewBag.Mensagem = "Os melhores alunos são aqueles que estudam mais!";
+            return View();
+           
+        }
+        public ActionResult RedirecionamentoEstudo()
+        {
+            return RedirectToAction("Aluno", "Aula");
+
+        }
+        public ActionResult RedirecionamentoGoogle()
+        { 
+         return Redirect("https://www.google.com/");
+        }
+    }
+}
